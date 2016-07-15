6ce62c0f63e24aae3fb55695df56dffef94b54fd
codeamac14 <codeamac14@gmail.com>
2016-07-15 18:37:27 -0500

    Final Twitter Normal

---
+++ notas_APItwitter.rb
@@ -0,0 +1,30 @@
+-- Definicion de API
+
+Sabes lo que es un API (Application Programming Interface)?
+
+Un API es una "librería" creada para poder comunicarse con un software en particular, 
+de manera sencilla. Normalmente esta librería contiene funciones, especificaciones y 
+procedimientos que te permites comunicarte con un software en particular para recibir 
+servicios del mismo.
+
+-- OAuth la libreria de twitter
+
+El API de Twitter esta basada en OAuth esta será la primera vez que utilices este protocolo. 
+OAuth es un protocolo de auntenticación que permite a usuarios poder aprobar a aplicaciones 
+para actuar por ellos sin tener que compartir su password. En esta app realmente no llegaremos 
+hasta este paso pero en un siguiente reto lo harás.
+
+OAuth-based application
+https://dev.twitter.com/oauth/overview/faq
+
+Twitter gem
+http://www.rubydoc.info/gems/twitter
+
+La configuración te pedirá que registres tu aplicación para obtener un API key y un API secret. 
+Entra a la siguiente ruta para hacerlo. 
+https://apps.twitter.com/app/new
+
+cache
+https://en.wikipedia.org/wiki/Cache_(computing)
+
+http://api.jquery.com/submit/
